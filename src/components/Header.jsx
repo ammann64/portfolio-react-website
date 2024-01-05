3edbe26301fde8f74f45ce17df964e9d26601ec0
ammann64 <austinmmann64@gmail.com>
2024-01-04 18:23:44 -0600

    React page switching, vite setup, and Header and NavTabs components added

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Nav from './NavTabs';
+
+export default function Header() {
+
+    return (
+        <header className="container-fluid">
+            <h1 className="col-md-6"></h1>
+            <Nav />
+        </header>
+    );
+}
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App.jsx';
+import About from './pages/About';
+import Error from './pages/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
